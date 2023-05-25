3719a860e70cc2b1aeb8d17b461a296da16505cb
noaione <noaione0809@gmail.com>
2023-05-25 11:02:19 +0800

    feat: support gunzip for log file

--- showtimes/tooling.py
+++ showtimes/tooling.py
@@ -17,6 +17,7 @@ If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import glob
+import gzip
 import inspect
 import logging
 import os
@@ -39,6 +40,7 @@ __all__ = (
     "get_logger",
 )
 ROOT_DIR = Path(__file__).absolute().parent
+logger = logging.getLogger("showtimes.tooling")
 
 
 class RollingFileHandler(RotatingFileHandler):
@@ -51,6 +53,7 @@ class RollingFileHandler(RotatingFileHandler):
     """
 
     maxBytes: int  # to force mypy to stop complaining????
+    gunzip: bool
 
     def __init__(
         self,
@@ -60,6 +63,7 @@ class RollingFileHandler(RotatingFileHandler):
         backupCount: int = 0,
         encoding: Optional[str] = None,
         delay: bool = False,
+        gunzip: bool = True,
     ) -> None:
         self._last_backup_count = 0
         super().__init__(
@@ -67,13 +71,17 @@ class RollingFileHandler(RotatingFileHandler):
         )
         self.maxBytes = maxBytes
         self.backupCount = backupCount
+        self.gunzip = gunzip
         self._determine_start_count()
 
     def _determine_start_count(self):
         all_files = glob.glob(self.baseFilename + "*")
         if all_files:
             all_files.sort()
-            last_digit = all_files[-1].split(".")[-1]
+            fn = all_files[-1]
+            if fn.endswith(".gz"):
+                fn = fn[:-3]
+            last_digit = fn.split(".")[-1]
             if last_digit.isdigit():
                 self._last_backup_count = int(last_digit)
 
@@ -85,6 +93,50 @@ class RollingFileHandler(RotatingFileHandler):
         self.rotate(self.baseFilename, next_name)
         if not self.delay:
             self.stream = self._open()
+
+    def _safe_gunzip(self, source: str, dest: str):
+        try:
+            with open(source, "rb") as sf:
+                with gzip.open(dest + ".gz", "wb") as df:
+                    for line in sf:
+                        df.write(line)
+            return True
+        except Exception as exc:
+            logger.error("Failed to gzip %s: %s", source, str(exc), exc_info=exc)
+            return False
+
+    def _safe_rename(self, source: str, dest: str):
+        try:
+            os.rename(source, dest)
+            return True
+        except Exception as exc:
+            logger.error("Failed to rename %s to %s: %s", source, dest, str(exc), exc_info=exc)
+            return False
+
+    def _safe_remove(self, source: str):
+        try:
+            os.remove(source)
+            return True
+        except Exception as exc:
+            logger.error("Failed to remove %s: %s", source, str(exc), exc_info=exc)
+            return False
+
+    def rotator(self, source: str, dest: str) -> None:
+        # Override the rotator to gzip the file before moving it
+        if not os.path.exists(source):
+            return  # silently fails
+        if self.gunzip:
+            # Try to gzip the file
+            result = self._safe_gunzip(source, dest)
+            if result:
+                # If successful, delete the original file
+                self._safe_remove(source)
+            else:
+                # If not successful, just rename the file
+                self._safe_rename(source, dest)
+        else:
+            # Just rename the file
+            self._safe_rename(source, dest)
 
 
 def setup_logger(log_path: Path):

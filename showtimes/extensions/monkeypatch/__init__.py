7987e496658022ec6be8153b0da18a67d8fc4e3d
noaione <noaione0809@gmail.com>
2023-08-06 12:27:44 +0800

    monkeypatch aiohttp to use certifi ssl context
    should solve some stupid issue with old stuff

---
+++ showtimes/extensions/monkeypatch/__init__.py
@@ -0,0 +1,10 @@
+"""
+showtimes.extensions.monkeypatch
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A collection of monkeypatch for external libraries that we are using.
+
+:copyright: (c) 2022-present naoTimes Project
+:license: AGPL-3.0, see LICENSE for more details.
+"""
+
+from .aiohttp import *
